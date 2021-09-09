b7c461b70f95474b2c2ab8e3898a98808a5fbceb
Gaeden Evans <gaedenevans@Gaedens-MacBook-Pro.local>
2021-09-08 18:07:35 -0700

    intial commit

---
+++ Lesson_py.py
@@ -0,0 +1,27 @@
+from BirdBrain import Finch
+
+bird = Finch()
+'''
+print('Excercise1')
+bird.setMove('F',25,100)
+bird.setMove('B',25,100)
+
+print('Excercise2')
+bird.setMove('F',25,100)
+bird.setMove('B',25,100)
+bird.setMove('F',25,100)
+bird.setMove('B',40,100)
+
+print('Excercise3')
+bird.setTurn('R',360,80)
+bird.setTurn('L',360,100)
+'''
+print('Excercise4')
+bird.setMove('F',25,100)
+bird.setTurn('L',90,100)
+bird.setMove('F',25,100)
+bird.setTurn('L',90,100)
+bird.setMove('F',25,100)
+bird.setTurn('L',90,100)
+bird.setMove('F',25,100)
+bird.setTurn('L',90,100)
